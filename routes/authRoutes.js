3eb36824c1f51864d02a3e3a311d5ee84f3c62fd
Yazan <yazanmatar90@gmail.com>
2019-08-07 17:54:17 -0400

    changed back the scope of google oauth

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -26,7 +26,7 @@ module.exports = app => {
   app.get(
     "/auth/google",
     passport.authenticate("google", {
-      scope: ["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email"]
+      scope: ["profile", "email"]
     })
   );
 
