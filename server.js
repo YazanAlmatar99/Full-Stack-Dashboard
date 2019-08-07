4d527b03b9e45cd72d2539465da1b149b61f2939
Yazan <yazanmatar90@gmail.com>
2019-08-07 17:59:57 -0400

    removed the middleware from the server.js file

--- server.js
+++ server.js
@@ -47,19 +47,19 @@ app.post('/api/world', (req, res) => {
     `I received your POST request. This is what you sent me: ${req.body.post}`,
   );
 });
-app.use(function(req, res, next) {
-  if (process.env.NODE_ENV === "production") {
-      const reqType = req.headers["x-forwarded-proto"];
-      // if not https redirect to https unless logging in using OAuth
-      if (reqType !== "https") {
-          req.url.indexOf("auth/google") !== -1
-            ? next()
-            : res.redirect("https://" + req.headers.host + req.url);
-      } 
-  } else {
-      next();
-  }
-});  
+// app.use(function(req, res, next) {
+//   if (process.env.NODE_ENV === "production") {
+//       const reqType = req.headers["x-forwarded-proto"];
+//       // if not https redirect to https unless logging in using OAuth
+//       if (reqType !== "https") {
+//           req.url.indexOf("auth/google") !== -1
+//             ? next()
+//             : res.redirect("https://" + req.headers.host + req.url);
+//       } 
+//   } else {
+//       next();
+//   }
+// });  
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
   app.use(express.static(path.join(__dirname, 'client/build')));
