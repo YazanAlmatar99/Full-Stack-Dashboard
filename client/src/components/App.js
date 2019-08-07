d2f18d65775e1005a13580f1e46ee4381bdc8972
Yazan <yazanmatar90@gmail.com>
2019-08-07 13:05:21 -0400

    updated the app.js file

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import Header from "./Header";
 import Dashboard from "./Dashboard";
-import { Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { connect } from "react-redux";
 import * as actions from '../actions'
-import { withRouter } from "react-router";
+
 class App extends React.Component {
 
   componentDidMount() {
@@ -13,21 +13,18 @@ class App extends React.Component {
 
   render() {
     return (
-      <Switch>
-          <Route exact path="/" component={Header} />
-          </Switch>
-      // <div className="container">
-      //   <Router>
-      //     <div>
-      //     <Route exact path="/" component={Header} />          
-      //     <Route exact path="/dashboard" component={Dashboard} />
-      //     </div>
-      //   </Router>
-      // </div>
+        <div className="container">
+        <Router>
+          <div>
+          <Route exact path="/" component={Header} />          
+          <Route exact path="/dashboard" component={Dashboard} />
+          </div>
+        </Router>
+      </div>
       // </div>
     );
   }
 }
 
 
-export default withRouter(connect(null,actions)(App));
+export default connect(null,actions)(App);
--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from './components/App';
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import reducers from "./reducers"; //reducers/index.js
+import reduxThunk from 'redux-thunk';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import * as serviceWorker from './serviceWorker';
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk))
+ReactDOM.render(
+    <Provider store={store}>
+    <App />
+  </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
