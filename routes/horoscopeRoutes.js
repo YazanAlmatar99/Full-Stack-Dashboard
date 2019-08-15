e0542138a6f02e7e29bfe39e0d72d3a1d46037c3
Yazan <yazanmatar90@gmail.com>
2019-08-15 17:19:35 -0400

    added more to the cors

--- routes/horoscopeRoutes.js
+++ routes/horoscopeRoutes.js
@@ -6,7 +6,7 @@ module.exports = app => {
   var whitelist = ['https://sleepy-cove-91820.herokuapp.com/']
   var corsOptions = {
     origin: function (origin, callback) {
-      if (whitelist.indexOf(origin) !== -1) {
+      if (whitelist.indexOf(origin) !== -1 || !origin) {
         callback(null, true)
       } else {
         callback(new Error('Not allowed by CORS'))
