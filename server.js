bd4751cd76600a769ef6ece11fa0c23e55dff6d7
Yazan <yazanmatar90@gmail.com>
2019-08-16 12:22:31 -0400

    added a middleware to the cors

--- server.js
+++ server.js
@@ -26,6 +26,12 @@ app.all('*', function(req, res, next) {
     next();
   }
 });
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 
 
 app.use(
